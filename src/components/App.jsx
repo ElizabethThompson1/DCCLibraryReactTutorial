8c30a62aeca4906ba035962978dccc01e89aeb29
ElizabethThompson1 <usawsoccer11@yahoo.com>
2021-11-11 10:49:33 -0600

    added multiple buttons and function that cycle the book selection

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,28 @@ class App extends Component {
             bookNumber: 0
         };
     }
+
+    goToNextrBook =() => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if (tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook =() => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if (tempBookNumber < 0){
+            tempBookNumber = this.books.length - 1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
     render(){
         return(
            <div className= "container-fluid">
@@ -22,6 +44,7 @@ class App extends Component {
                <div className = "row">
                    <div className = "col-md-4">
                    {/*Button here to move to the previous book viewed*/}
+                   <button onClick= {this.goToPreviousBook}>Previous Book</button>
                </div>
                <div className = "col-md-4">
                    {/*Display book with cover here*/}
@@ -30,8 +53,9 @@ class App extends Component {
                </div>
                <div className = "col-md-4">
                    {/*Button here to move to the next book viewed*/}
+                   <button onClick = {this.goToNextrBook}>Next Book</button>
                </div>  
-               </div>
+            </div>
               
            </div>
         )
